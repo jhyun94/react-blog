12e2a4df11bbd30e26db8e566c696033b8374c5d
jhyun94 <hyunjonathan@gmail.com>
2018-03-23 12:19:43 -0500

    able to submit a form on post new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
 
-	render(){
+	inputField(field){
+		console.log(field)
 		return (
-			<Link
-				to='/'
-				className="btn btn-danger">
-				Cancel
-			</Link>
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+				className="form-control" type="text" {...field.input}/>
+			</div>
+		)
+	}
+
+	onSubmit(values){
+		console.log(values);
+	}
+
+	render(){
+		const { handleSubmit } = this.props;
+		return (
+			<div>
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+					<Field
+						label='Title'
+						name="title"
+						component={this.inputField}
+					/>
+					<Field
+						label='Categories'
+						name="categories"
+						component={this.inputField}
+					/>
+					<Field
+						label='Content'
+						name="content"
+						component={this.inputField}
+					/>
+					<button type="submit" className="btn btn-primary">Submit</button>
+					<Link to='/'className="btn btn-danger">
+						Cancel
+					</Link>
+				</form>
+			</div>
 		)
 	}
 }
 
-export default PostsNew;
+
+
+export default reduxForm({
+	form: 'post'
+})(PostsNew);
+
+
+
