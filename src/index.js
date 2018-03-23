96e500e4020276b23be8065203ff97427aab5dec
jhyun94 <hyunjonathan@gmail.com>
2018-03-22 23:04:19 -0500

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reducer from './reducers/index';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
@@ -8,16 +11,18 @@ import PostsNew from './components/posts_new';
 
 
 // const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+const store = createStore(reducer);
 
 
 ReactDOM.render(
-	<BrowserRouter>
-		<div>
-			<Switch>
-				<Route path='/posts/new' component={PostsNew}/>
-				<Route path='/' component={PostsIndex}/>
-			</Switch>
-		</div>
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route path='/posts/new' component={PostsNew}/>
+					<Route path='/' component={PostsIndex}/>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>
   , document.querySelector('.container'));
