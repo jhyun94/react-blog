b22e79fd2aad4cd93aa64fd7b93147e87c84cc29
jhyun94 <hyunjonathan@gmail.com>
2018-03-22 22:29:50 -0500

    posts new boilerplate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 
 
@@ -13,6 +14,7 @@ import PostsIndex from './components/posts_index';
 ReactDOM.render(
 	<BrowserRouter>
 		<Switch>
+			<Route path='/posts/new' component={PostsNew}/>
 			<Route path='/' component={PostsIndex}/>
 		</Switch>
 	</BrowserRouter>
