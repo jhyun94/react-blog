2e2b2b6d398768685beb63e96711d2bfe07f0880
jhyun94 <hyunjonathan@gmail.com>
2018-03-22 23:06:22 -0500

    link back to home

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
 	render(){
 		return (
-			<div>PostsNew</div>
+			<Link
+				to='/'
+				className="btn btn-danger">
+				Cancel
+			</Link>
 		)
 	}
 }
