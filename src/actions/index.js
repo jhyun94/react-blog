023164fbacc8cfebbf31c3489ab17b23e1a1ecb1
jhyun94 <hyunjonathan@gmail.com>
2018-03-23 21:20:34 -0500

    reducer for fetch posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
 const API_KEY = '?key=jonjon1';
+
 const CREATE_POST = 'create_post';
+const FETCH_POSTS = 'fetch_posts';
+
 
 export function createPost(values){
 	const request = axios.post(`${ROOT_URL}${API_KEY}`, values);
@@ -10,4 +13,14 @@ export function createPost(values){
 		type: CREATE_POST,
 		payload: request
 	};
+}
+
+export function fetchPosts(){
+	const request = axios.get(`${ROOT_URL}${API_KEY}`);
+
+	return {
+		type: FETCH_POSTS,
+		payload: request
+	}
+
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+import { fetchPosts } from '../actions';
 class PostsIndex extends Component {
 
 	render(){
